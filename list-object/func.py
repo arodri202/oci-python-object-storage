19ca5855b2ea130604be8d0a8dbd30a4254ac028
arodri202 <angelrodriguez99202@gmail.com>
2019-07-15 12:14:22 -0700

    Licensing

--- list-object/func.py
+++ list-object/func.py
@@ -1,3 +1,4 @@
+# Copyright (c) 2016, 2018, Oracle and/or its affiliates.  All rights reserved.
 import io
 import json
 import os
--- put-object/rp.py
+++ put-object/rp.py
@@ -1,3 +1,4 @@
+# Copyright (c) 2016, 2018, Oracle and/or its affiliates.  All rights reserved.
 import base64
 import json
 import os.path
